81fcc0ebd38a776ad796f258a1468e9a415fef9f
Renato Groffe <renato.groffe@yahoo.com.br>
2018-05-01 21:00:32 -0300

    ASP.NET Core 2.0 + JWT + Refresh Tokens + Redis
    ASP.NET Core 2.0 + JWT + Refresh Tokens + Redis

---
+++ APIAlturas/APIAlturas/Classes.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+namespace APIAlturas
+{
+    public class AccessCredentials
+    {
+        public string UserID { get; set; }
+        public string AccessKey { get; set; }
+        public string RefreshToken { get; set; }
+        public string GrantType { get; set; }
+    }
+
+    public class User
+    {
+        public string UserID { get; set; }
+        public string AccessKey { get; set; }
+    }
+
+    public class RefreshTokenData
+    {
+        public string RefreshToken { get; set; }
+        public string UserID { get; set; }
+    }
+
+    public class TokenConfigurations
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public int Seconds { get; set; }
+        public int FinalExpiration { get; set; }
+    }
+
+    public class SigningConfigurations
+    {
+        public SecurityKey Key { get; }
+        public SigningCredentials SigningCredentials { get; }
+
+        public SigningConfigurations()
+        {
+            using (var provider = new RSACryptoServiceProvider(2048))
+            {
+                Key = new RsaSecurityKey(provider.ExportParameters(true));
+            }
+
+            SigningCredentials = new SigningCredentials(
+                Key, SecurityAlgorithms.RsaSha256Signature);
+        }
+    }
+}
---
+++ APIAlturas/APIAlturas/Controllers/LoginController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace APIAlturas.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public object Post(
+            [FromBody]AccessCredentials credenciais,
+            [FromServices]UsersDAO usersDAO,
+            [FromServices]SigningConfigurations signingConfigurations,
+            [FromServices]TokenConfigurations tokenConfigurations,
+            [FromServices]IDistributedCache cache)
+        {
+            bool credenciaisValidas = false;
+            if (credenciais != null && !String.IsNullOrWhiteSpace(credenciais.UserID))
+            {
+                if (credenciais.GrantType == "password")
+                {
+                    var usuarioBase = usersDAO.Find(credenciais.UserID);
+                    credenciaisValidas = (usuarioBase != null &&
+                        credenciais.UserID == usuarioBase.UserID &&
+                        credenciais.AccessKey == usuarioBase.AccessKey);
+                }
+                else if (credenciais.GrantType == "refresh_token")
+                {
+                    if (!String.IsNullOrWhiteSpace(credenciais.RefreshToken))
+                    {
+                        RefreshTokenData refreshTokenBase = null;
+
+                        string strTokenArmazenado =
+                            cache.GetString(credenciais.RefreshToken);
+                        if (!String.IsNullOrWhiteSpace(strTokenArmazenado))
+                        {
+                            refreshTokenBase = JsonConvert
+                                .DeserializeObject<RefreshTokenData>(strTokenArmazenado);
+                        }
+
+                        credenciaisValidas = (refreshTokenBase != null &&
+                            credenciais.UserID == refreshTokenBase.UserID &&
+                            credenciais.RefreshToken == refreshTokenBase.RefreshToken);
+
+                        // Elimina o token de refresh já que um novo será gerado
+                        if (credenciaisValidas)
+                            cache.Remove(credenciais.RefreshToken);
+                    }
+
+                }
+            }
+
+            if (credenciaisValidas)
+            {
+                return GenerateToken(
+                    credenciais.UserID, signingConfigurations,
+                    tokenConfigurations, cache);
+            }
+            else
+            {
+                return new
+                {
+                    authenticated = false,
+                    message = "Falha ao autenticar"
+                };
+            }
+        }
+
+        private object GenerateToken(string userID,
+            SigningConfigurations signingConfigurations,
+            TokenConfigurations tokenConfigurations,
+            IDistributedCache cache)
+        {
+            ClaimsIdentity identity = new ClaimsIdentity(
+                new GenericIdentity(userID, "Login"),
+                new[] {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                        new Claim(JwtRegisteredClaimNames.UniqueName, userID)
+                }
+            );
+
+            DateTime dataCriacao = DateTime.Now;
+            DateTime dataExpiracao = dataCriacao +
+                TimeSpan.FromSeconds(tokenConfigurations.Seconds);
+            
+            // Calcula o tempo máximo de validade do refresh token
+            // (o mesmo será invalidado automaticamente pelo Redis)
+            TimeSpan finalExpiration =
+                TimeSpan.FromSeconds(tokenConfigurations.FinalExpiration);
+
+            var handler = new JwtSecurityTokenHandler();
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = tokenConfigurations.Issuer,
+                Audience = tokenConfigurations.Audience,
+                SigningCredentials = signingConfigurations.SigningCredentials,
+                Subject = identity,
+                NotBefore = dataCriacao,
+                Expires = dataExpiracao
+            });
+            var token = handler.WriteToken(securityToken);
+
+            var resultado = new
+            {
+                authenticated = true,
+                created = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                expiration = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss"),
+                accessToken = token,
+                refreshToken = Guid.NewGuid().ToString().Replace("-", String.Empty),
+                message = "OK"
+            };
+
+            // Armazena o refresh token em cache através do Redis 
+            var refreshTokenData = new RefreshTokenData();
+            refreshTokenData.RefreshToken = resultado.refreshToken;
+            refreshTokenData.UserID = userID;
+
+            DistributedCacheEntryOptions opcoesCache =
+                new DistributedCacheEntryOptions();
+            opcoesCache.SetAbsoluteExpiration(finalExpiration);
+            cache.SetString(resultado.refreshToken,
+                JsonConvert.SerializeObject(refreshTokenData),
+                opcoesCache);
+
+            return resultado;
+        }
+    }
+}
